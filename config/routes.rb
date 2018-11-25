b9606b385e1e080b92878d4018bd39e8efbb5fd2
Raghu Betina <raghu@firstdraft.com>
2018-11-25 23:23:07 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "races_homepages#index"
   # Routes for the Event resource:
 
