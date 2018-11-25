8f7f1cb76057931f2af37b0cf404c567ae61f5f1
Raghu Betina <raghu@firstdraft.com>
2018-11-25 23:22:49 +0000

    Direct association between users and races

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,6 +1,10 @@
 class Race < ApplicationRecord
   # Direct associations
 
+  belongs_to :participant,
+             :class_name => "User",
+             :foreign_key => "user_id"
+
   has_many   :workouts_homepages,
              :class_name => "RacesHomepage",
              :dependent => :destroy
