c6575365e50a1a59f47b7066ead5af69d4b1c84a
Raghu Betina <raghu@firstdraft.com>
2018-11-25 23:23:36 +0000

    Only race user can modify race

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,4 +1,14 @@
 class RacesController < ApplicationController
+  before_action :current_user_must_be_race_participant, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_race_participant
+    race = Race.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == race.participant
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @races = Race.all
 
