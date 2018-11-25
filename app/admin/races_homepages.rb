88acb55cb21d201be1735bc56b2dff00e3d0d7e0
Raghu Betina <raghu@firstdraft.com>
2018-11-25 23:23:10 +0000

    Whitelist columns in RacesHomepage for ActiveAdmin

--- app/admin/races_homepages.rb
+++ app/admin/races_homepages.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register RacesHomepage do
+
+ permit_params :race_id, :user_id, :location_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
